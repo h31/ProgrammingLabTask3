34f0e859aed765d5acad9e25ee23d51ce7d9e311
GalaXYDY <dannyglxy@gmail.com>
2018-04-23 23:25:31 +0300

    Aram zam zam

--- app/src/main/java/com/planetofheroes/amarioforester/poh/Splash.java
+++ app/src/main/java/com/planetofheroes/amarioforester/poh/Splash.java
@@ -29,7 +29,7 @@ public class Splash extends AppCompatActivity {
         Thread timer = new Thread() {
             public void run () {
                 try {
-                    sleep(3500);
+                    sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
