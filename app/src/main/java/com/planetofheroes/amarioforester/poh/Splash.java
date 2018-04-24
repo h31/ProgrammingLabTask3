619edb23aa42c91204d1683770dcbf4fa7edc5cd
GalaXYDY <dannyglxy@gmail.com>
2018-04-24 15:06:17 +0300

    Aram zam zam

--- app/src/main/java/com/planetofheroes/amarioforester/poh/Splash.java
+++ app/src/main/java/com/planetofheroes/amarioforester/poh/Splash.java
@@ -12,30 +12,29 @@ import android.widget.Toast;
 
 public class Splash extends AppCompatActivity {
 
-    private TextView tv ;
-    private ImageView iv ;
+    private TextView tv;
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        tv = (TextView) findViewById(R.id.tv) ;
-        iv = (ImageView) findViewById(R.id.iv) ;
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
 
 
-        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
         tv.startAnimation(myanim);
         iv.startAnimation(myanim);
         final Intent i = new Intent(this, MainActivity.class);
         Thread timer = new Thread() {
-            public void run () {
+            public void run() {
                 try {
                     sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
-                }
-                finally {
+                } finally {
                     startActivity(i);
                     finish();
                 }
@@ -44,12 +43,12 @@ public class Splash extends AppCompatActivity {
         timer.start();
     }
 
-    @Override //masha
+    @Override                                                //masha - non-clickable back button
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(keyCode==KeyEvent.KEYCODE_BACK)
-            Toast.makeText(getApplicationContext(), "loading...",
-                    Toast.LENGTH_LONG).show();
+        if (keyCode == KeyEvent.KEYCODE_BACK)
+            Toast.makeText(getApplicationContext(), "",
+                    Toast.LENGTH_LONG).getView();
         return false;
     }
-    }
+}
 
