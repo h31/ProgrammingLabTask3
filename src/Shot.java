3cf2019d2b7de4ab24d9f89f6cd5b520cabf3ae1
Violetta-Mamykina <violetta-1998m@mail.ru>
2018-05-01 17:05:44 +0300

    added Shot

---
+++ src/Shot.java
@@ -0,0 +1,24 @@
+public class Shot {
+
+    private int x;
+    private int y;
+    private boolean shot;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+    public boolean isShot() {
+        return shot;
+    }
+
+    Shot (int x, int y, boolean shot) {
+        this.x = x;
+        this.y = y;
+        this.shot = shot;
+    }
+}
