914bfb9cfc8dcfb749f68c51b85ee60f1091a740
Amario Forester <maschalessnichaya@gmail.com>
2018-05-05 19:49:08 +0300

    Blargh hero gallery

---
+++ app/src/main/java/com/planetofheroes/amarioforester/poh/Hero.java
@@ -0,0 +1,17 @@
+package com.planetofheroes.amarioforester.poh;
+
+public class Hero {
+    private int mImageDrawable;
+
+    public Hero(int mImageDrawable) {
+        this.mImageDrawable = mImageDrawable;
+    }
+
+    public int getmImageDrawable() {
+        return mImageDrawable;
+    }
+
+    public void setmImageDrawable(int mImageDrawable) {
+        this.mImageDrawable = mImageDrawable;
+    }
+}
--- app/src/main/java/com/planetofheroes/amarioforester/poh/HeroesActivity.java
+++ app/src/main/java/com/planetofheroes/amarioforester/poh/HeroesActivity.java
@@ -1,13 +1,28 @@
 package com.planetofheroes.amarioforester.poh;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class HeroesActivity extends MainActivity {
+
+    private ListView listView;
+    private HeroAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heroes);
+
+        listView = (ListView) findViewById(R.id.heroes_list);
+        ArrayList<Hero> heroesList = new ArrayList<>();
+        heroesList.add(new Hero(R.drawable.beardlayer));
+        heroesList.add(new Hero(R.drawable.bubbleslayer));
+        heroesList.add(new Hero(R.drawable.duncanlayer));
+        heroesList.add(new Hero(R.drawable.sofialayer));
+        heroesList.add(new Hero(R.drawable.timmylayer));
+
+        mAdapter = new HeroAdapter(this, heroesList);
+        listView.setAdapter(mAdapter);
     }
 }
