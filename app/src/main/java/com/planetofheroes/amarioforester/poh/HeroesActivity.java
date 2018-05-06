ea288375dacd9cef5b999d3150fc7c747f25bcc9
Amario Forester <maschalessnichaya@gmail.com>
2018-05-06 18:58:32 +0300

    reset mathefaka

--- app/src/main/java/com/planetofheroes/amarioforester/poh/HeroesActivity.java
+++ app/src/main/java/com/planetofheroes/amarioforester/poh/HeroesActivity.java
@@ -1,28 +1,13 @@
 package com.planetofheroes.amarioforester.poh;
 
 import android.os.Bundle;
-import android.widget.ListView;
-import java.util.ArrayList;
+
 
 public class HeroesActivity extends MainActivity {
-
-    private ListView listView;
-    private HeroAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heroes);
-
-        listView = (ListView) findViewById(R.id.heroes_list);
-        ArrayList<Hero> heroesList = new ArrayList<>();
-        heroesList.add(new Hero(R.drawable.beardlayer));
-        heroesList.add(new Hero(R.drawable.bubbleslayer));
-        heroesList.add(new Hero(R.drawable.duncanlayer));
-        heroesList.add(new Hero(R.drawable.sofialayer));
-        heroesList.add(new Hero(R.drawable.timmylayer));
-
-        mAdapter = new HeroAdapter(this, heroesList);
-        listView.setAdapter(mAdapter);
     }
 }
