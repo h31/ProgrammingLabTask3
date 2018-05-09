b62f4017c7024891c457cd080f47e8d5aa74646e
Violetta-Mamykina <violetta-1998m@mail.ru>
2018-05-09 19:11:24 +0300

    Begin 2.0

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,73 +1,28 @@
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-
-import java.util.Random;
 
 public class Battleship extends Application {
 
-    boolean gameOver;
-    AllShips shipsOfComputer;
-    AllShots shotsOfComputer;
-    AllShips shipsOfUser;
-    AllShots shotsOfUser;
-    Random randomShot;
-
-    void distribution() {
-        shipsOfComputer = new AllShips(60, true);
-        shotsOfComputer = new AllShots(60);
-        shipsOfUser = new AllShips(60, false);
-        shotsOfUser = new AllShots(60);
-        gameOver = false;
-    }
-
-    @Override
-    public void start(Stage windowOfComputer) throws Exception {
-        Group root = new Group();
-        windowOfComputer.setTitle("Battleship");
-        windowOfComputer.setScene(new Scene(root, 600, 600, Color.WHITE));
-        Canvas canvas = new Canvas(600, 600);
-        root.getChildren().add(canvas);
-        GraphicsContext lines = canvas.getGraphicsContext2D();
-        int sizeOfCell = 60;
-        for (int i = 1; i < 10; i++) {
-            lines.beginPath();
-            lines.setStroke(Color.LIGHTBLUE);
-            lines.moveTo(0, i * sizeOfCell);
-            lines.lineTo(10 * sizeOfCell, i * sizeOfCell);
-            lines.stroke();
-            lines.moveTo(i * sizeOfCell, 0);
-            lines.lineTo(i * sizeOfCell, 10 * sizeOfCell);
-            lines.stroke();
-        }
-        windowOfComputer.show();
-    }
-
-    public void click(MouseEvent event) {
-        this.click(event);
-        int x = (int) event.getX() / 60;
-        int y = (int) event.getY() / 60;
-        if (event.getButton() == MouseButton.PRIMARY && !gameOver) {
-            if (!shotsOfUser.hitSamePlace(x, y)) {
-                shotsOfUser.addShot(x, y, true);
-                if (shipsOfComputer.checkHit(x, y)) {
-                    if (!shipsOfComputer.checkAlive()) {
-                        gameOver = true; // say User win
-                    }
-                }
+    public void start(Stage computerStage) {
+        GridPane root = new GridPane();
+        Scene scene = new Scene (root, 600, 600);
+        computerStage.setTitle("Buttleship");
+        computerStage.setScene(scene);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                Cell cell = new Cell();
+                cell.getStylesheets().add(Battleship.class.getResource("/StyleForBattleship.css").toExternalForm());
+                cell.setMinSize(60, 60);
+                cell.setOnMouseClicked(event -> {
+                    cell.setDisable(false);
+                    cell.setStyle("-fx-background-color: rgba(34, 85, 85, 0.13);");
+                });
+                cell.setCellType(CellType.COMMON);
+                root.add(cell, i, j);
             }
-            else computerShot(); 
         }
-
+        computerStage.show();
     }
-    public void computerShot() {
-
-    }
-
 }
--- src/Cell.java
+++ src/Cell.java
@@ -1,43 +1,23 @@
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Button;
 
 
-public class Cell {
+public class Cell extends Button {
 
-    private int x;
-    private int y;
-    private Color color;
+    private CellType cellType;
 
-    public int getX() {
-        return x;
+    Cell() {
+        this.cellType = cellType;
     }
 
-    public int getY() {
-        return y;
+    Cell(CellType cellType) {
+        this.cellType = cellType;
     }
 
-    Cell(int x, int y) {
-        this.x = x;
-        this.y = y;
-        color = Color.GRAY;
+    public void setCellType(CellType cellType) {
+        this.cellType = cellType;
     }
 
-    boolean isHit(int x, int y) {
-        if (this.x == x && this.y == y) {
-            color = Color.RED;
-            return true;
-        }
-        return false;
-    }
-
-    boolean isAlive() {
-        return color != Color.RED;
-    }
-
-    void drawingGridlines(GraphicsContext gridlines, int sizeOfCell, boolean hide) {
-        if (!hide || color == Color.RED) {
-            gridlines.setFill(color);
-            gridlines.fillRect(x*sizeOfCell+1, y*sizeOfCell+1, sizeOfCell-2, sizeOfCell-2);
-        }
+    public CellType getCellType() {
+        return cellType;
     }
 }
