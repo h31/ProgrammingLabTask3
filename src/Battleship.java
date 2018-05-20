a30ac7fb0a10b5846335b532d63e743fd1aa6dd0
Violetta-Mamykina <violetta-1998m@mail.ru>
2018-05-20 19:36:16 +0300

    added setCellType changed the style

--- src/Battleship.java
+++ src/Battleship.java
@@ -3,26 +3,5 @@ import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class Battleship extends Application {
-
-    public void start(Stage computerStage) {
-        GridPane root = new GridPane();
-        Scene scene = new Scene (root, 600, 600);
-        computerStage.setTitle("Buttleship");
-        computerStage.setScene(scene);
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                Cell cell = new Cell();
-                cell.getStylesheets().add(Battleship.class.getResource("/StyleForBattleship.css").toExternalForm());
-                cell.setMinSize(60, 60);
-                cell.setOnMouseClicked(event -> {
-                    cell.setDisable(false);
-                    cell.setStyle("-fx-background-color: rgba(34, 85, 85, 0.13);");
-                });
-                cell.setCellType(CellType.COMMON);
-                root.add(cell, i, j);
-            }
-        }
-        computerStage.show();
-    }
+public class Battleship {
 }
--- src/Cell.java
+++ src/Cell.java
@@ -5,19 +5,60 @@ public class Cell extends Button {
 
     private CellType cellType;
 
-    Cell() {
-        this.cellType = cellType;
+    private void setCellType() {
+        switch (cellType) {
+            case EMPTY:
+                this.getStyleClass().add("empty");
+                this.setId("empty");
+                break;
+            case SHIP:
+                this.getStyleClass().add("user_ship");
+                this.setId("user_ship");
+                this.setDisable(false);
+                break;
+            case INJURED_SHIP:
+                this.getStyleClass().add("injured_ship");
+                this.setId("injured_ship");
+                break;
+            case MISS:
+                this.getStyleClass().add("miss");
+                this.setId("miss");
+                break;
+            case HIDDEN_SHIP:
+                this.getStyleClass().add("hidden_ship");
+                this.setId("hidden_ship");
+                break;
+        }
     }
 
-    Cell(CellType cellType) {
-        this.cellType = cellType;
+    private void setCellType(int cellType) {
+        switch (cellType) {
+            case 0:
+                this.cellType = CellType.EMPTY;
+                break;
+            case 1:
+                this.cellType = CellType.SHIP;
+                break;
+            case 2:
+                this.cellType = CellType.INJURED_SHIP;
+                break;
+            case 3:
+                this.cellType = CellType.MISS;
+                break;
+            case 4:
+                this.cellType = CellType.HIDDEN_SHIP;
+                break;
+        }
+        setCellType();
     }
 
-    public void setCellType(CellType cellType) {
-        this.cellType = cellType;
+
+    public Cell(int x, int y, int cellType, int size) {
+        this.setMinSize(size, size);
+        setCellType(cellType);
     }
 
-    public CellType getCellType() {
-        return cellType;
+    public Cell (int x, int y, int cellType) {
+        this(x, y, cellType, 60);
     }
 }
