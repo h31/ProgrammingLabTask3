aed494c5ee82bc6f04fcf51d5ae1f4a296c82c1e
farru <farrukh.sharipov99@mail.ru>
2018-06-30 01:48:33 +0300

    Farrukh Sharipov,3 task

--- src/src/Window.java
+++ src/src/Window.java
@@ -1,21 +1,17 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
  * Created by infuntis on 15/01/17.
  */
 public class Window extends JFrame {
         public JPanel jPanel = new JPanel();
-        private JButton jButton1 = new JButton("Новая игра");
 
     public Window(){
         setTitle("Змейка");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(320,336);
         setLocation(500,200);
-        add(new Field());
+        add(new Field2());
         ImageIcon icon = new ImageIcon("C:\\Users\\farru\\IdeaProjects\\ProgrammingLabTask3\\src\\src\\App-ksnake-icon.png");
         setIconImage(icon.getImage());
         setVisible(true);
