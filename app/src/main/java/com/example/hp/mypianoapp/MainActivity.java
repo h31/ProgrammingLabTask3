d10cb27ca21892f2590e9218f0e30f13e25645f4
skwlkr <alex.moran.free@gmail.com>
2018-07-05 20:53:24 +0300

    some changes

--- app/src/main/java/com/example/hp/mypianoapp/MainActivity.java
+++ app/src/main/java/com/example/hp/mypianoapp/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         float currentVolumeIndex = (float) audioManager.getStreamVolume(streamType);
 
 
-        float maxVolumeIndex  = (float) audioManager.getStreamMaxVolume(streamType);
+        float maxVolumeIndex = (float) audioManager.getStreamMaxVolume(streamType);
 
 
         this.volume = currentVolumeIndex / maxVolumeIndex;
@@ -54,20 +54,18 @@ public class MainActivity extends AppCompatActivity {
         this.setVolumeControlStream(streamType);
 
 
-        if (Build.VERSION.SDK_INT >= 21 ) {
+        if (Build.VERSION.SDK_INT >= 21) {
 
             AudioAttributes audioAttrib = new AudioAttributes.Builder()
                     .setUsage(AudioAttributes.USAGE_GAME)
                     .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
                     .build();
 
-            SoundPool.Builder builder= new SoundPool.Builder();
+            SoundPool.Builder builder = new SoundPool.Builder();
             builder.setAudioAttributes(audioAttrib).setMaxStreams(MAX_STREAMS);
 
             this.soundPool = builder.build();
-        }
-
-        else {
+        } else {
 
             this.soundPool = new SoundPool(MAX_STREAMS, AudioManager.STREAM_MUSIC, 0);
         }
@@ -81,29 +79,29 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        this.soundIdC = this.soundPool.load(this, R.raw.c,1);
+        this.soundIdC = this.soundPool.load(this, R.raw.c, 1);
 
-        this.soundIdD = this.soundPool.load(this, R.raw.d,1);
+        this.soundIdD = this.soundPool.load(this, R.raw.d, 1);
 
-        this.soundIdE = this.soundPool.load(this, R.raw.e,1);
+        this.soundIdE = this.soundPool.load(this, R.raw.e, 1);
 
-        this.soundIdF = this.soundPool.load(this, R.raw.f,1);
+        this.soundIdF = this.soundPool.load(this, R.raw.f, 1);
 
-        this.soundIdG = this.soundPool.load(this, R.raw.g,1);
+        this.soundIdG = this.soundPool.load(this, R.raw.g, 1);
 
-        this.soundIdA = this.soundPool.load(this, R.raw.a,1);
+        this.soundIdA = this.soundPool.load(this, R.raw.a, 1);
 
-        this.soundIdB = this.soundPool.load(this, R.raw.b,1);
+        this.soundIdB = this.soundPool.load(this, R.raw.b, 1);
 
-        this.soundIdCis = this.soundPool.load(this, R.raw.cis,1);
+        this.soundIdCis = this.soundPool.load(this, R.raw.cis, 1);
 
-        this.soundIdDis = this.soundPool.load(this, R.raw.dis,1);
+        this.soundIdDis = this.soundPool.load(this, R.raw.dis, 1);
 
-        this.soundIdFis = this.soundPool.load(this, R.raw.fis,1);
+        this.soundIdFis = this.soundPool.load(this, R.raw.fis, 1);
 
-        this.soundIdGis = this.soundPool.load(this, R.raw.gis,1);
+        this.soundIdGis = this.soundPool.load(this, R.raw.gis, 1);
 
-        this.soundIdAis = this.soundPool.load(this, R.raw.ais,1);
+        this.soundIdAis = this.soundPool.load(this, R.raw.ais, 1);
 
     }
 
@@ -123,83 +121,83 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void playSoundD(View view){
-        if(loaded)  {
+    public void playSoundD(View view) {
+        if (loaded) {
             float leftVolumn = volume;
             float rightVolumn = volume;
-            int streamId = this.soundPool.play(this.soundIdD,leftVolumn, rightVolumn, 1, 0, 1f);
+            int streamId = this.soundPool.play(this.soundIdD, leftVolumn, rightVolumn, 1, 0, 1f);
         }
     }
 
-    public void playSoundDis(View view){
-        if(loaded)  {
+    public void playSoundDis(View view) {
+        if (loaded) {
             float leftVolumn = volume;
             float rightVolumn = volume;
-            int streamId = this.soundPool.play(this.soundIdDis,leftVolumn, rightVolumn, 1, 0, 1f);
+            int streamId = this.soundPool.play(this.soundIdDis, leftVolumn, rightVolumn, 1, 0, 1f);
         }
     }
 
-    public void playSoundE(View view){
-        if(loaded)  {
+    public void playSoundE(View view) {
+        if (loaded) {
             float leftVolumn = volume;
             float rightVolumn = volume;
-            int streamId = this.soundPool.play(this.soundIdE,leftVolumn, rightVolumn, 1, 0, 1f);
+            int streamId = this.soundPool.play(this.soundIdE, leftVolumn, rightVolumn, 1, 0, 1f);
         }
     }
 
-    public void playSoundF(View view){
-        if(loaded)  {
+    public void playSoundF(View view) {
+        if (loaded) {
             float leftVolumn = volume;
             float rightVolumn = volume;
-            int streamId = this.soundPool.play(this.soundIdF,leftVolumn, rightVolumn, 1, 0, 1f);
+            int streamId = this.soundPool.play(this.soundIdF, leftVolumn, rightVolumn, 1, 0, 1f);
         }
     }
 
-    public void playSoundFis(View view){
-        if(loaded)  {
+    public void playSoundFis(View view) {
+        if (loaded) {
             float leftVolumn = volume;
             float rightVolumn = volume;
-            int streamId = this.soundPool.play(this.soundIdFis,leftVolumn, rightVolumn, 1, 0, 1f);
+            int streamId = this.soundPool.play(this.soundIdFis, leftVolumn, rightVolumn, 1, 0, 1f);
         }
     }
 
-    public void playSoundG(View view){
-        if(loaded)  {
+    public void playSoundG(View view) {
+        if (loaded) {
             float leftVolumn = volume;
             float rightVolumn = volume;
-            int streamId = this.soundPool.play(this.soundIdG,leftVolumn, rightVolumn, 1, 0, 1f);
+            int streamId = this.soundPool.play(this.soundIdG, leftVolumn, rightVolumn, 1, 0, 1f);
         }
     }
 
-    public void playSoundGis(View view){
-        if(loaded)  {
+    public void playSoundGis(View view) {
+        if (loaded) {
             float leftVolumn = volume;
             float rightVolumn = volume;
-            int streamId = this.soundPool.play(this.soundIdGis,leftVolumn, rightVolumn, 1, 0, 1f);
+            int streamId = this.soundPool.play(this.soundIdGis, leftVolumn, rightVolumn, 1, 0, 1f);
         }
     }
 
-    public void playSoundA(View view){
-        if(loaded)  {
+    public void playSoundA(View view) {
+        if (loaded) {
             float leftVolumn = volume;
             float rightVolumn = volume;
-            int streamId = this.soundPool.play(this.soundIdA,leftVolumn, rightVolumn, 1, 0, 1f);
+            int streamId = this.soundPool.play(this.soundIdA, leftVolumn, rightVolumn, 1, 0, 1f);
         }
     }
 
-    public void playSoundAis(View view){
-        if(loaded)  {
+    public void playSoundAis(View view) {
+        if (loaded) {
             float leftVolumn = volume;
             float rightVolumn = volume;
-            int streamId = this.soundPool.play(this.soundIdAis,leftVolumn, rightVolumn, 1, 0, 1f);
+            int streamId = this.soundPool.play(this.soundIdAis, leftVolumn, rightVolumn, 1, 0, 1f);
         }
     }
 
-    public void playSoundB(View view){
-        if(loaded)  {
+    public void playSoundB(View view) {
+        if (loaded) {
             float leftVolumn = volume;
             float rightVolumn = volume;
-            int streamId = this.soundPool.play(this.soundIdB,leftVolumn, rightVolumn, 1, 0, 1f);
+            int streamId = this.soundPool.play(this.soundIdB, leftVolumn, rightVolumn, 1, 0, 1f);
         }
     }
 
