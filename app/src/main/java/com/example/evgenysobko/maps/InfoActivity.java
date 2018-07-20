e612804581c91ec2e18437ea973d1d45417bc4db
evgenysobko <jooohn127@gmail.com>
2018-07-20 10:36:20 +0500

    ¯\_(ツ)_/¯

--- app/src/main/java/com/example/evgenysobko/maps/InfoActivity.java
+++ app/src/main/java/com/example/evgenysobko/maps/InfoActivity.java
@@ -1,15 +1,8 @@
 package com.example.evgenysobko.maps;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.Marker;
 
 public class InfoActivity extends AppCompatActivity {
 
